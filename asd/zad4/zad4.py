e1d40ce3c1fcf3c357fa7049e349d9ceea109b21
Rellikeht <michaelhemperek@gmail.com>
2023-04-22 23:16:27 +0200

    starting to do something

--- asd/zad4/zad4.py
+++ asd/zad4/zad4.py
@@ -1,11 +1,53 @@
 #!/usr/bin/env python
 from zad4testy import runtests
+from queue import Queue
+
+
+def shortest_path(G, s, t):
+    visited = [False for _ in range(len(G))]
+    vs = Queue()
+    cp = None
+
+    visited[s] = True
+    for v in G[s]:
+        visited[v] = True
+        vs.put([s, v])
+
+    while True:
+        cp = vs.get()
+        last = cp[-1]
+
+        if last == t:
+            break
+
+        for v in G[last]:
+            if not visited[v]:
+                np = cp.copy()
+                visited[v] = True
+                np.append(v)
+                vs.put(np)
+
+    path = []
+    for i in range(1, len(cp)):
+        path.append([cp[i-1], cp[i]])
+    return path
+
+
+def without_edge(G, e):
+    without = G.copy()
+    without[e[0]].remove(e[1])
+    without[e[1]].remove(e[0])
+    return without
 
 
 def longer(G, s, t):
-    # tu prosze wpisac wlasna implementacje
-    return (0, 0)
+    slen = shortest_path(G, s, t)
+
+    for e in slen:
+        if len(shortest_path(without_edge(G, e), s, t)) > len(slen):
+            return e
+    return None
 
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests(longer, all_tests=False)
+runtests(longer, all_tests=True)
