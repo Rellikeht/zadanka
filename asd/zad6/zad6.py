c11570422dc65ee1b709339a15c7e24d509cac47
Rellikeht <michaelhemperek@gmail.com>
2023-05-22 09:35:11 +0200

    fuck that algorithmic shit

--- asd/zad6/zad6.py
+++ asd/zad6/zad6.py
@@ -1,8 +1,117 @@
+#!/usr/bin/env python
 from zad6testy import runtests
+from queue import Queue
 
-def binworker( M ):
-    # tu prosze wpisac wlasna implementacje
-    return 0
+INF = float("inf")
+
+
+def binworker(M):
+    NIL = len(M)
+    STARTDIST = INF
+    H = [[] for _ in M]
+
+    for ms in range(len(M)):
+        for m in M[ms]:
+            H[m].append(ms)
+
+    mdist = [STARTDIST for _ in M] + [STARTDIST]  # 0 for NIL
+    hdist = [STARTDIST for _ in M] + [STARTDIST]  # like before
+
+    q = Queue()
+
+    # /*
+    #  G = U ∪ V ∪ {NIL}
+    #  where U and V are the left and right sides of the bipartite graph and
+    #  NIL is a special null vertex
+    # */
+
+    #  1 function BFS() is
+    #  2     for each u in U do
+    #  3         if Pair_U[u] = NIL then
+    #  4             Dist[u] := 0
+    #  5             Enqueue(Q, u)
+    #  6         else
+    #  7             Dist[u] := ∞
+    #  8     Dist[NIL] := ∞
+    #  9     while Empty(Q) = false do
+    # 10         u := Dequeue(Q)
+    # 11         if Dist[u] < Dist[NIL] then
+    # 12             for each v in Adj[u] do
+    # 13                 if Dist[Pair_V[v]] = ∞ then
+    # 14                     Dist[Pair_V[v]] := Dist[u] + 1
+    # 15                     Enqueue(Q, Pair_V[v])
+    # 16     return Dist[NIL] ≠ ∞
+
+    def BFS():
+        for m in range(len(M)):
+            if mpair[m] is None:
+                mdist[m] = 0
+                q.put(m)
+            else:
+                mdist[m] = INF
+
+        mdist[NIL] = INF
+        while not q.empty():
+
+            m = q.get()
+            if mdist[m] < mdist[NIL]:
+                for h in M[m]:
+                    if hdist[hpair[h]] == INF:
+                        hdist[hpair[h]] = mdist[m] + 1
+                        q.put(hpair[h])
+
+        return hdist[NIL] != INF
+
+    #  1 function DFS(u) is
+    #  2     if u ≠ NIL then
+    #  3         for each v in Adj[u] do
+    #  4             if Dist[Pair_V[v]] = Dist[u] + 1 then
+    #  5                 if DFS(Pair_V[v]) = true then
+    #  6                     Pair_V[v] := u
+    #  7                     Pair_U[u] := v
+    #  8                     return true
+    #  9         Dist[u] := ∞
+    # 10         return false
+    # 11     return true
+
+    def DFS(m):
+        if m is not None:
+            for h in range(len(H)):
+                if mdist[hpair[h]] == mdist[m]+1:
+                    if DFS(hpair[h]):
+                        hpair[h] = m
+                        mpair[m] = h
+                        return True
+
+            mdist[m] = INF
+            return False
+        return True
+
+    #  1 function Hopcroft–Karp is
+    #  2     for each u in U do
+    #  3         Pair_U[u] := NIL
+    #  4     for each v in V do
+    #  5         Pair_V[v] := NIL
+    #  6     matching := 0
+    #  7     while BFS() = true do
+    #  8         for each u in U do
+    #  9             if Pair_U[u] = NIL then
+    # 10                 if DFS(u) = true then
+    # 11                     matching := matching + 1
+    # 12     return matching
+
+    mpair = [None for _ in M]
+    hpair = [None for _ in M]
+    matching = 0
+
+    while BFS():
+        for m in range(len(M)):
+            if mpair[m] is None:
+                if DFS(m):
+                    matching += 1
+
+    return matching
+
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( binworker, all_tests = False )
+runtests(binworker, all_tests=False)
