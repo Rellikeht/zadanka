0538d94424dc843e8cff39e82ccb83fdee7364a7
Rellikeht <michaelhemperek@gmail.com>
2024-03-07 15:00:48 +0100

    finally ?

--- sysopy/cw02/zad1/main.c
+++ sysopy/cw02/zad1/main.c
@@ -9,21 +9,43 @@ int main(int argc, char **argv) {
     if (num == 0) {
       fprintf(stderr,
               "Jako pierwszy argument podać poprawną liczbę większą od 0\n");
-      return 42;
+      return 1;
     }
     if (argc > 2) {
       max_iter = atoi(argv[2]);
       if (max_iter == 0) {
         fprintf(stderr,
                 "Jako drugi argument podać poprawną liczbę większą od 0\n");
-        return 255;
+        return 2;
       }
     }
   }
 
-  printf("%d is ", num);
+#ifdef dynlib
+#include <dlfcn.h>
+  void *handle = dlopen("libcollatz.so", RTLD_LAZY);
+  if (!handle) {
+    fprintf(stderr, "Błąd z ładowniem biblioteki\n");
+    return 3;
+  }
+
+  int (*test_collatz_convergence)(int, int);
+  test_collatz_convergence =
+      (int (*)(int, int))dlsym(handle, "test_collatz_convergence");
+
+  char *err = dlerror();
+  if (err != NULL) {
+    fprintf(stderr, "De El Error: %s\n", err);
+    return 4;
+  }
+#endif
+
+  printf("%d ", num);
   if (test_collatz_convergence(num, max_iter) == -1)
-    printf("not ");
-  printf("collatz convergent in %d steps\n", max_iter);
-  return 0;
+    printf("nie ");
+  printf("jest zbieżny do 1 w %d krokach\n", max_iter);
+
+#ifdef dynlib
+  dlclose(handle);
+#endif
 }
