c9d303871b068ccd0f5e051dbb56fa3de9a32480
Rellikeht <michaelhemperek@gmail.com>
2024-03-18 13:10:09 +0100

    starts to look a little less ugly

--- sysopy/cw03/zad1/main.c
+++ sysopy/cw03/zad1/main.c
@@ -8,6 +8,14 @@
     return code;                                                               \
   }
 
+#define transfer(amount)                                                       \
+  fread(in, sizeof(char), amount, file);                                       \
+  doOrErr(111, ferror(file), "Problem przy odczycie pliku %s: %i\n", fname,    \
+          err);                                                                \
+  for (i = 0; i < amount; i++)                                                 \
+    out[i] = in[amount - i - 1];                                               \
+  fwrite(out, sizeof(char), amount, stdout)
+
 int main(int argc, char *argv[]) {
   int err;
   char *fname;
@@ -40,37 +48,23 @@ int main(int argc, char *argv[]) {
   doOrErr(2, fseek(file, -1, SEEK_END),
           "Problem z przesuwaniem wskaźnika: %i\n", errno);
 
-  while (ftell(file) >= BSIZE) {
+  if (ftell(file) >= BSIZE) {
     doOrErr(3, fseek(file, -BSIZE, SEEK_CUR),
             "Problem z przesuwaniem wskaźnika: %i\n", errno);
-    fread(in, sizeof(char), BSIZE, file);
-    fwrite(out, sizeof(char), BSIZE, stdout);
   }
 
-  int amount = ftell(file);
-  doOrErr(4, fseek(file, 0, SEEK_SET), "Problem z przesuwaniem wskaźnika: %i\n",
+  while (ftell(file) >= BSIZE) {
+    doOrErr(4, fseek(file, -BSIZE, SEEK_CUR),
+            "Problem z przesuwaniem wskaźnika: %i\n", errno);
+    transfer(BSIZE);
+    doOrErr(5, fseek(file, -BSIZE, SEEK_CUR),
+            "Problem z przesuwaniem wskaźnika: %i\n", errno);
+  }
+
+  int amount = ftell(file) + 1;
+  doOrErr(6, fseek(file, 0, SEEK_SET), "Problem z przesuwaniem wskaźnika: %i\n",
           errno);
-  fread(in, sizeof(char), amount, file);
-  for (i = 0; i < amount; i++)
-    in[i] = out[amount - i];
-  fwrite(out, sizeof(char), amount, stdout);
-
-  /* do { */
-
-  /*   amount = fread(in, sizeof(char), BSIZE, file); */
-  /*   doOrErr(3, ferror(file), "Problem z odczytem pliku %s: %i\n", fname,
-   * err); */
-
-  /*   for (i = 0; i < amount; i++) */
-  /*     out[i] = in[i]; */
-  /*   /1*   out[amount - i] = in[i]; *1/ */
-
-  /*   fwrite(out, sizeof(char), amount, stdout); */
-  /*   fseek(file, -2 * amount, SEEK_CUR); */
-  /*   /1* } while (ftell(file) >= BSIZE); *1/ */
-
-  /* doOrErr(4, ferror(file), "Problem z odczytem pliku %s: %i\n", fname, err);
-   */
+  transfer(amount);
 
 #endif
 
