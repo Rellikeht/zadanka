fe4b9161f2b7fa0a52f53c6b0309476dfcf578fd
Rellikeht <michaelhemperek@gmail.com>
2024-04-03 18:52:28 +0200

    almost

--- sysopy/cw04/zad1/main.c
+++ sysopy/cw04/zad1/main.c
@@ -1,12 +1,10 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
-/* #include <sys/stat.h> */
-/* #include <sys/types.h> */
 
 #define doOrErr(code, op, args...)                                             \
   err = op;                                                                    \
@@ -18,23 +16,29 @@
 int main(int argc, char *argv[]) {
   int err, amount, i;
   pid_t parent_pid, child_pid;
-  doOrErr(1, argc < 2, "Należy podać ilość procesów\n");
+  doOrErr(1, argc != 2, "Zła ilość procesów\n");
   amount = atoi(argv[1]);
   doOrErr(2, amount == 0, "Należy podać liczbę > 0\n");
 
   parent_pid = getpid();
   for (i = 0; i < amount; i++) {
     child_pid = fork();
-    doOrErr(3, child_pid == -1, "Nie udało się wykonać fork(), errno: %i\n",
+    doOrErr(3, child_pid == -1, "Nie udało się wykonać fork(), kod błędu: %i\n",
             errno);
     if (child_pid == 0) {
-      printf("rodzic: %i , dziecko: %i\n", (int)parent_pid, (int)getpid());
+      printf("rodzic: %i , dziecko: %i\n", parent_pid, getpid());
       return 0;
     }
   }
 
-  while (wait(0) != -1) {
+  while (true) {
+    if (wait(0) == -1) {
+      doOrErr(4, errno != 0 && errno != ECHILD, "Błąd w wait(), jego kod: %i\n",
+              errno);
+      break;
+    }
   };
-  printf("Całkowita ilość procesów potomnych: %i\n", amount);
+
+  printf("Ilość procesów potomnych: %i\n", amount);
   return 0;
 }
--- sysopy/cw04/zad2/main.c
+++ sysopy/cw04/zad2/main.c
@@ -1,61 +1,77 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<dirent.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+/* #include<sys/stat.h> */
+/* #include<sys/types.h> */
+
+#define doOrErr(code, op, args...)                                             \
+  err = op;                                                                    \
+  if (err) {                                                                   \
+    fprintf(stderr, args);                                                     \
+    return code;                                                               \
+  }
+
+#define BUFSIZE 1024
 int global;
 
-int main(int argc, char *argv[]){
-    int local = 0; 
-    char path[500];
+int main(int argc, char *argv[]) {
+  int local = 0, err;
+  char buf[BUFSIZE + 1] = {0};
+  DIR *directory;
+  pid_t parent_pid, child_pid, child_exit_pid;
+  int child_status, ls_status;
+  char *path;
 
-    if (argc != 2) {
-        printf("Invalid number of arguments./n");
-        return -1;
-    }
+  doOrErr(1, argc != 2, "Zła ilość procesów\n");
+  directory = opendir(argv[1]);
 
-    DIR *directory = opendir(argv[1]);
-    if (directory == NULL) {
-        printf("Invalid path. Using current directory instead.\n");
-        closedir(directory);
-        getcwd(path, 200);
-        directory = opendir(path);
-    }
-    else {
-        strcpy(path, argv[1]);
-    }
-    printf("Directory path: %s\n", path);
+  if (directory == NULL) {
+    printf("Zła ścieżka. Używam obecnej zamiast niej.\n");
+    path = getcwd(buf, BUFSIZE);
+    doOrErr(2, path == NULL, "getcwd() się nie udało, kod błędu: %i\n", errno);
+    directory = opendir(path);
+    doOrErr(3, directory == NULL,
+            "Nie udało się otworzyć obecnej ścieżki, kod błędu: %i\n", errno);
+  } else {
+    strncpy(buf, argv[1], BUFSIZE);
+  }
+  printf("Directory path: %s\n", argv[1]);
 
-    pid_t parent_pid, child_pid;
-    parent_pid = getpid();
-    child_pid = fork();
+  parent_pid = getpid();
+  child_pid = fork();
+  doOrErr(4, child_pid == -1, "Nie udało się wykonać fork(), kod błędu: %i\n",
+          errno);
 
-    if (child_pid > 0) {
-        printf("parent process\n");
-        printf("parent pid = %d, child pid = %d\n", (int) parent_pid, (int) child_pid);
-        int child_status;
-        waitpid(child_pid, &child_status, 0);
-        printf("Child exit code: %d\n", child_status);
-        printf("Parent's local = %d, parent's global = %d\n", local, global);
-        return 1;
-    }
-    else if (child_pid == 0) {
-        printf("child process\n");
-        local++;
-        global++;
-        printf("child pid = %d, parent pid = %d\n", (int) getpid(), (int) parent_pid);
-        printf("Child's local = %d, childs's global = %d\n", local, global);
-        int ls_status = execl("/bin/ls", "ls", path, (char*) NULL);
-        return ls_status;
-    }
-    else {
-        printf("Creation of child process was unsuccessful.\n");
-    }
+  if (child_pid > 0) {
+    printf("Rodzic: \n");
+    printf("pid rodzica: %i, pid dziecka: = %i\n", parent_pid, child_pid);
+    child_exit_pid = waitpid(child_pid, &child_status, 0);
+    doOrErr(5, child_exit_pid != child_pid,
+            "Problem przy waitpid(), kod błędu: %i\n", errno);
+    doOrErr(6, child_status != 0, "Proces potomny zakończył się z kodem %i\n",
+            child_status);
+    printf("Kod wyjściowy procesu potomnego: %d\n", child_status);
+    printf("local rodzica to %i, global rodzica to %i\n", local, global);
 
-    closedir(directory);
-    return 1;
+  } else {
+    printf("Proces potomny:\n");
+    local++;
+    global++;
+    printf("pid procesu potomnego = %i, pid rodzica = %i\n", getpid(),
+           parent_pid);
+    printf("local procesu potomnego to %i, global procesu potomnego to %i\n",
+           local, global);
+    // Tu coś nie gra
+    /* ls_status = execl("/usr/bin/ls", "ls", path, NULL); */
+    ls_status = execl("/usr/bin/ls", path, NULL);
+    return ls_status;
+  }
+
+  closedir(directory);
+  return 0;
 }
