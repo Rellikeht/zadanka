5ed59c70ef8bdc0a0edd65e6598f9c6287bc484d
Rellikeht <michaelhemperek@gmail.com>
2024-04-09 12:59:20 +0200

    what a shit

--- sysopy/cw05/zad2/catcher.c
+++ sysopy/cw05/zad2/catcher.c
@@ -1,18 +1,51 @@
-#include <errno.h>
+#include "errors.c"
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#define doOrErr(code, op, args...)                                             \
-  err = op;                                                                    \
-  if (err) {                                                                   \
-    fprintf(stderr, args);                                                     \
-    return code;                                                               \
-  }
+const char *my_name = "catcher";
+int err = 0;
+union sigval val = {0};
+pid_t sender_pid = 0;
+
+void resend(int sig, siginfo_t *info, void *context) {
+  err = 0;
+  err = sigqueue(sender_pid, SIGUSR1, val);
+}
+
+void work(int sig, siginfo_t *info, void *context) {}
 
 int main(int argc, char *argv[]) {
-  /* int err = 0; */
-  /* char *name; */
-  printf("Catcher: PID = %i\n", getpid());
+  void *previous = NULL;
+  sigset_t usr1mask = {0};
+  struct sigaction receive = {0}, action = {0};
+  receive.sa_flags = SA_SIGINFO;
+  receive.sa_sigaction = resend;
+
+  action.sa_flags = SA_SIGINFO;
+  receive.sa_sigaction = work;
+
+  err = sigemptyset(&usr1mask);
+  if (err != 0) {
+    errp();
+    return 1;
+  }
+  err = sigaddset(&usr1mask, SIGUSR1);
+  if (err != 0) {
+    errp();
+    return 1;
+  }
+
+  printf("%s: PID = %i\n", my_name, getpid());
+  while (true) {
+    err = sigaction(SIGUSR1, &receive, NULL);
+    /* TODO ???? */
+    if (err != 0) {
+      errp();
+      return 2;
+    }
+  }
 
   return 0;
 }
--- sysopy/cw05/zad2/sender.c
+++ sysopy/cw05/zad2/sender.c
@@ -1,17 +1,54 @@
-#include <errno.h>
-#include <stdio.h>
+#include "errors.c"
+#include <limits.h>
+#include <signal.h>
+#include <stdlib.h>
 
-#define doOrErr(code, op, args...)                                             \
-  err = op;                                                                    \
-  if (err) {                                                                   \
-    fprintf(stderr, args);                                                     \
-    return code;                                                               \
-  }
+const char *my_name = "sender";
+int err = 0;
+union sigval val = {0};
 
 int main(int argc, char *argv[]) {
-  int err = 0;
-  char *name;
-  doOrErr(1, argc != 2, "Zła ilość argumentów\n");
+  char *invalid = 0;
+  long catcher_pid = 0, sig_type = 0;
+
+  if (argc != 3) {
+    eprint("Zła ilość argumentów\n");
+    return 1;
+  }
+
+  catcher_pid = strtol(argv[1], &invalid, 10);
+  if (catcher_pid == LONG_MIN || catcher_pid == LONG_MAX || *invalid != 0) {
+    eprint("Błąd przy konwersji pidu catchera (argv[1])\n");
+    return 1;
+  } else if (catcher_pid < 2) {
+    /* 2 bo 1 to init, więc catcher nie może mieć takiego pid */
+    eprint("pid catchera (argv[1]) musi być większy od 1\n");
+    return 1;
+  }
+
+  sig_type = strtol(argv[2], &invalid, 10);
+  if (sig_type == LONG_MIN || sig_type == LONG_MAX || *invalid != 0) {
+    eprint("Błąd przy konwersji trybu pracy (argv[2])\n");
+    return 1;
+  } else if (sig_type < 1 || sig_type > 3) {
+    eprint("Niepoprawny tryb pracy: %li\n", sig_type);
+    return 1;
+  }
+
+  err = kill(catcher_pid, SIGUSR1);
+  if (err != 0) {
+    errp();
+    return 2;
+  }
+
+  /* TODO odbiór i sprawdzenie */
+
+  val.sival_int = sig_type;
+  err = sigqueue(catcher_pid, SIGUSR1, val);
+  if (err != 0) {
+    errp();
+    return 2;
+  }
 
   return 0;
 }
