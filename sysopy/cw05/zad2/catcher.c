1870134c7364042111f2ba93935e18281eed2c93
Rellikeht <michaelhemperek@gmail.com>
2024-04-10 10:32:02 +0200

    all works?

--- sysopy/cw05/zad2/catcher.c
+++ sysopy/cw05/zad2/catcher.c
@@ -9,23 +9,26 @@
 #define BUFSIZE 2048
 const char *my_name = "catcher";
 char command[BUFSIZE] = {0};
-int err = 0, err2 = 0;
+int err = 0, err2 = 0, requests = 0;
 union sigval val = {0};
-pid_t sender_pid = 0, my_pid = 0;
+pid_t my_pid = 0;
 bool running = true;
 
 void resend(int sig, siginfo_t *info, void *context) {
   printf("%s: Odebrano SIGUSR1\n", my_name);
   err2 = kill(info->si_pid, SIGUSR1);
-  printf("%s: Wysłano SIGUSR1\n", my_name);
 }
 
 void work(int sig, siginfo_t *info, void *context) {
-  printf("%s: Ciągle praca, brak zabawy...\n", my_name);
+  /* printf("%s: Ciągle praca, brak zabawy...\n", my_name); */
+  requests++;
   switch (info->si_value.sival_int) {
   case 1:
+    for (int i = 0; i < 100; i++)
+      printf("%s: %i\n", my_name, i);
     break;
   case 2:
+    printf("%s: Otrzymał już %i żądań\n", my_name, requests);
     break;
   case 3:
     running = false;
@@ -59,70 +62,49 @@ int main(int argc, char *argv[]) {
 
   receive.sa_flags = SA_SIGINFO;
   receive.sa_sigaction = resend;
-  /* receive.sa_mask = usr1mask; */
-  err = sigemptyset(&receive.sa_mask);
-  if (err != 0) {
-    errp();
-    return 1;
-  }
-
+  receive.sa_mask = usr1mask;
   action.sa_flags = SA_SIGINFO;
   action.sa_sigaction = work;
-  /* action.sa_mask = usr1mask; */
-  err = sigemptyset(&action.sa_mask);
-  if (err != 0) {
-    errp();
-    return 1;
-  }
+  action.sa_mask = usr1mask;
 
   my_pid = getpid();
   printf("%s: PID = %i\n", my_name, my_pid);
   sprintf(command, "echo -n %i | xclip -i -selection clipboard", my_pid);
   system(command);
 
-  /* while (running) { */
-  err = sigaction(SIGUSR1, &receive, NULL);
-  if (err != 0) {
-    errp();
-    return 2;
-  }
-  printf("%s: receive\n", my_name);
+  while (running) {
+    err = sigaction(SIGUSR1, &receive, NULL);
+    if (err != 0) {
+      errp();
+      return 2;
+    }
 
-  err = sigsuspend(&usr1mask);
-  if (err2 != 0) {
-    errp();
-    return 3;
-  }
-  if (err != -1 && errno != EINTR) {
-    errp();
-    return 4;
-  }
-  printf("%s: suspend\n", my_name);
+    err = sigsuspend(&usr1mask);
+    if (err2 != 0) {
+      errp();
+      return 3;
+    }
+    if (err != -1 && errno != EINTR) {
+      errp();
+      return 4;
+    }
 
-  err = sigaction(SIGUSR1, &action, NULL);
-  if (err != 0) {
-    errp();
-    return 5;
-  }
+    err = sigaction(SIGUSR1, &action, NULL);
+    if (err != 0) {
+      errp();
+      return 5;
+    }
 
-  err = usleep(10000);
-  if (err != 0) {
-    errp();
-    return 5;
+    err = sigsuspend(&usr1mask);
+    if (err2 != 0) {
+      errp();
+      return 6;
+    }
+    if (err != -1 && errno != EINTR) {
+      errp();
+      return 7;
+    }
   }
-  printf("%s: action\n", my_name);
-
-  err = sigsuspend(&usr1mask);
-  if (err2 != 0) {
-    errp();
-    return 6;
-  }
-  if (err != -1 && errno != EINTR) {
-    errp();
-    return 7;
-  }
-  printf("%s: suspend 2\n", my_name);
-  /* } */
 
   return 0;
 }
