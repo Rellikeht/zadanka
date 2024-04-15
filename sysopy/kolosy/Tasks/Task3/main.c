a156b4ab6e219e24ef20fd0ff3e7f673b14af6ea
Rellikeht <michaelhemperek@gmail.com>
2024-04-15 21:04:07 +0200

    what is going on

--- sysopy/kolosy/Tasks/Task3/main.c
+++ sysopy/kolosy/Tasks/Task3/main.c
@@ -17,19 +17,86 @@ int main(int argc, char **argv) {
     pipe(fd);
     pid_t pid = fork();
     if (pid == 0) {
-      //  zamknij deskryptor do zapisu i wykonaj program sort na filename1
-      //  w przypadku błędu zwróć 3
-
+      //  zamknij deskryptor do zapisu
       err = close(fd[1]);
       if (err != 0) {
         perror("close()");
         return 3;
       }
+
+      //  i wykonaj program sort na filename1
       err = sprintf(command, "sort %s", filename1);
       if (err < 0) {
         perror("sprintf()");
         return 3;
       }
+      err = 0;
+
+      err = system(command);
+      if (err != 0) {
+        if (errno == 0)
+          fprintf(stderr, "system() returned code %i\n", err);
+        else
+          perror("system()");
+        return 3;
+      }
+      //  w przypadku błędu zwróć 3
+
+      //  koniec
+    } else {
+      err = close(fd[0]);
+      if (err != 0) {
+        perror("close()");
+        return 3;
+      }
+    }
+  } else if (argc == 3) {
+    char *filename1 = argv[1];
+    char *filename2 = argv[2];
+    int fd[2];
+    //  otwórz plik filename2 z prawami dostępu rwxr--r--,
+    //  jeśli plik istnieje otwórz go i usuń jego zawartość
+    int f2 = open(filename2, O_CREAT | O_TRUNC,
+                  S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IROTH);
+    if (f2 < 0) {
+      perror("open()");
+      return 1;
+    }
+
+    //  koniec
+    pipe(fd);
+    pid_t pid = fork();
+    if (pid == 0) {
+      //  zamknij deskryptor do zapisu,
+      err = close(fd[1]);
+      if (err != 0) {
+        perror("close()");
+        return 1;
+      }
+
+      //  przekieruj deskryptor standardowego wyjścia na deskryptor pliku
+      //  filename2
+      int save = dup(f2);
+      if (dup2(fileno(stdout), f2) == -1) {
+        perror("cannot redirect stdout");
+        return 3;
+      }
+
+      // i zamknij plik,
+      err = close(f2);
+      if (err != 0) {
+        perror("close()");
+        return 3;
+      }
+
+      //  wykonaj program sort na filename1 w
+      err = sprintf(command, "sort %s", filename1);
+      if (err < 0) {
+        perror("sprintf()");
+        return 3;
+      }
+      err = 0;
+
       err = system(command);
       if (err != 0) {
         if (errno == 0)
@@ -39,26 +106,17 @@ int main(int argc, char **argv) {
         return 3;
       }
 
-      //  koniec
-    } else {
-      close(fd[0]);
-    }
-  } else if (argc == 3) {
-    char *filename1 = argv[1];
-    char *filename2 = argv[2];
-    int fd[2];
-    //  otwórz plik filename2 z prawami dostępu rwxr--r--,
-    //  jeśli plik istnieje otwórz go i usuń jego zawartość
+      if (fflush(stdout) != 0) {
+        perror("fflush()");
+        return 3;
+      }
 
-    //  koniec
-    pipe(fd);
-    pid_t pid = fork();
-    if (pid == 0) {
-      //  zamknij deskryptor do zapisu,
-      //  przekieruj deskryptor standardowego wyjścia na deskryptor pliku
-      //  filename2 i zamknij plik, wykonaj program sort na filename1 w
-      //  przypadku błędu zwróć 3.
+      if (close(save) != 0) {
+        perror("fflush()");
+        return 3;
+      }
 
+      //  w przypadku błędu zwróć 3
       //  koniec
     } else {
       close(fd[0]);
@@ -68,3 +126,18 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+/* if (access(filename2, F_OK) == 0) { */
+/*   err = chmod(filename2, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP |
+ * S_IROTH); */
+/*   if (err != 0) { */
+/*     perror("chmod()"); */
+/*     return 1; */
+/*   } */
+/* } */
+
+/* f2 = fopen(filename2, "w"); */
+/* if (f2 == NULL) { */
+/*   perror("fopen()"); */
+/*   return 1; */
+/* } */
