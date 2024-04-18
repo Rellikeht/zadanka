f11cf6618c3ae75023490b68a8592d0ff413febf
Rellikeht <michaelhemperek@gmail.com>
2024-04-18 16:48:36 +0200

    💀

--- sysopy/cw06/zad1/main.c
+++ sysopy/cw06/zad1/main.c
@@ -1,17 +1,22 @@
 #include <errno.h>
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+#define BUFSIZE 20
 
 int main(int argc, char *argv[]) {
     errno = 0;
     char *invalid = NULL;
+    char buf[BUFSIZE + 1] = {0};
     int err = 0;
 
     int i = 0;
     pid_t child = 0;
-    double sum = 0;
+    double sum = 0, a = 0, b = 0;
 
     if (argc != 3) {
         fprintf(stderr, "Liczba argumentów powinna wynosić 3\n");
@@ -36,6 +41,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Ilość procesów (argv[1]) musi być większa od 0\n");
         return 1;
     }
+    int pipes[processes][2];
 
     // TODO pipe
 
@@ -46,10 +52,49 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         if (child != 0) {
-            // parent
+            err = close(pipes[i][1]);
+            if (err == -1) {
+                fprintf(stderr, "Parent, write pipe %i: ", i);
+                perror("close()");
+                return 1;
+            }
             continue;
         }
     }
 
+    while (true) {
+        if (wait(0) == -1) {
+            if (errno != ECHILD) {
+                perror("wait()"); // ???
+                return 1;
+            }
+            break;
+        }
+    }
+
+    for (i = 0; i < processes; i++) {
+        err = read(pipes[i][0], buf, BUFSIZE);
+        if (err == -1) {
+            fprintf(stderr, "Parent, read pipe %i: ", i);
+            perror("read()");
+            return 1;
+        }
+
+        err = close(pipes[i][0]);
+        if (err == -1) {
+            fprintf(stderr, "Parent, read pipe %i: ", i);
+            perror("close()");
+            return 1;
+        }
+
+        a = strtod(buf, &invalid);
+        if (invalid != NULL) {
+            fprintf(stderr, "Parent, read pipe %i: Invalid double to parse\n",
+                    i);
+            return 1;
+        }
+    }
+
+    printf("Wartość całki: %lf\n", sum);
     return 0;
 }
