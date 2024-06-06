a1468dd7f5b902f8ebc23fd50d16ab6c254b5eb5
Rellikeht <michaelhemperek@gmail.com>
2024-06-06 09:52:09 +0200

    :(

--- sysopy/cw11/additional/client.c
+++ sysopy/cw11/additional/client.c
@@ -1,102 +1,103 @@
 #include <arpa/inet.h>
-#include <netinet/in.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
 
-#define MESSAGE_SIZE 512
-// Struktura do przekazania informacji do wątków
-typedef struct {
-    int client_socket_descriptor;
-} thread_data_t;
+#define UNUSED(x)          (void)(x)
+#define BUFFER_SIZE        1024
+#define MAX_IDENTIFIER_LEN 63
 
-// Funkcja wątku do odczytu wiadomości z serwera
-void *read_from_server(void *arg) {
-    thread_data_t *data = (thread_data_t *)arg;
-    int client_socket_descriptor =
-        data->client_socket_descriptor;
-    char response[MESSAGE_SIZE];
+int server_socket;
+char client_id[MAX_IDENTIFIER_LEN];
 
+void *receive_messages(void *a) {
+    UNUSED(a);
+
+    char buff[BUFFER_SIZE];
     while (1) {
-        memset(response, 0, sizeof(response));
-        int bytes_read = read(
-            client_socket_descriptor,
-            response,
-            sizeof(response) - 1
-        );
-        if (bytes_read > 0) {
-            printf("%s\n", response);
-        } else if (bytes_read < 0) {
-            perror("Error while reading from server");
-            close(client_socket_descriptor);
-            pthread_exit(NULL);
+        int received = recv(server_socket, buff, BUFFER_SIZE, 0);
+        if (received <= 0) {
+            break;
+        }
+        if (strncmp(buff, "ALIVE", 5) != 0) {
+            buff[received] = '\0';
+            printf("%s\n", buff);
         }
     }
+    return NULL;
+}
+
+void cleanup() {
+    send(server_socket, "STOP", 4, 0);
+    close(server_socket);
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        printf("Usage: %s ADDRESS PORT ID\n", argv[0]);
-        exit(1);
+    if (argc != 4) {
+        fprintf(
+            stderr,
+            "Usage: %s <client_id> <server_ip> <server_port>\n",
+            argv[0]
+        );
+        return 1;
     }
-    char *IP_ADDRESS = argv[1];
-    int PORT = atoi(argv[2]);
-    char *CLIENT_ID = argv[3];
 
-    int client_socket_descriptor =
-        socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket_descriptor < 0) {
-        perror("Error while creating socket");
-        exit(1);
+    strcpy(client_id, argv[1]);
+    char *server_ip = argv[2];
+    int server_port = atoi(argv[3]);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("socket");
+        return 1;
     }
 
     struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-    server_addr.sin_port = htons(PORT);
-
-    connect(
-        client_socket_descriptor,
-        (struct sockaddr *)&server_addr,
-        sizeof(server_addr)
-    );
-    char message[MESSAGE_SIZE];
-    sprintf(message, "SETID %s", CLIENT_ID);
-    write(
-        client_socket_descriptor, message, strlen(message) + 1
-    );
-
-    pthread_t read_thread;
-    thread_data_t thread_data;
-    thread_data.client_socket_descriptor =
-        client_socket_descriptor;
-
-    // Utworzenie wątku do odczytu z serwera
-    if (pthread_create(
-            &read_thread,
-            NULL,
-            read_from_server,
-            (void *)&thread_data
-        ) != 0) {
-        perror("Error creating thread");
-        close(client_socket_descriptor);
-        exit(1);
+    server_addr.sin_port = htons(server_port);
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        return 1;
     }
 
+    if (connect(
+            server_socket,
+            (struct sockaddr *)&server_addr,
+            sizeof(server_addr)
+        ) == -1) {
+        perror("connect");
+        return 1;
+    }
+
+    send(server_socket, client_id, strlen(client_id), 0);
+
+    signal(SIGINT, cleanup);
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, receive_messages, NULL);
+
+    char buff[BUFFER_SIZE];
     while (1) {
-        char user_input[256];
-        fgets(user_input, 256, stdin);
-        // sprintf(message, "%s> %s", CLIENT_ID, user_input);
-        write(
-            client_socket_descriptor,
-            user_input,
-            strlen(user_input) + 1
-        );
+        fgets(buff, BUFFER_SIZE, stdin);
+        buff[strcspn(buff, "\n")] = '\0';
+
+        if (strncmp(buff, "LIST", 4) == 0 ||
+            strncmp(buff, "STOP", 4) == 0) {
+            send(server_socket, buff, strlen(buff), 0);
+            if (strncmp(buff, "STOP", 4) == 0) {
+                break;
+            }
+        } else if (strncmp(buff, "2ALL", 4) == 0 || strncmp(buff, "2ONE", 4) == 0) {
+            send(server_socket, buff, strlen(buff), 0);
+        } else {
+            printf("Unknown command\n");
+        }
     }
-    close(client_socket_descriptor);
+
+    close(server_socket);
+    return 0;
 }
--- sysopy/cw11/additional/server.c
+++ sysopy/cw11/additional/server.c
@@ -1,317 +1,220 @@
-#include "command_utils.h"
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <poll.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
-/*
-    Sources used:
-    https://www.geeksforgeeks.org/socket-programming-cc/
-    https://man7.org/linux/man-pages/man2/poll.2.html
-*/
+volatile int g_keep_alive = 1;
 
+#define UNUSED(x)          (void)(x)
 #define MAX_CLIENTS        10
-#define CLIENT_ID_SIZE     256
-#define MESSAGE_SIZE       256
-#define TOTAL_MESSAGE_SIZE 512
-#define SETID_COMMAND      "SETID"
-#define TO_ALL_COMMAND     "2ALL"
-#define LIST_COMMAND       "LIST\n"
-#define TO_ONE_COMMAND     "2ONE"
-#define STOP_COMMAND       "STOP\n"
+#define BUFFER_SIZE        1024
+#define MAX_IDENTIFIER_LEN 63
 
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Usage: %s IP_ADDRESS PORT\n", argv[0]);
-        exit(1);
-    }
-    int PORT = atoi(argv[2]);
-    char *IP_ADDRESS = argv[1];
-    /* socket(domain, type, protocol) - creates a new socket and
-       returns its descriptor
-        - domain: AF_INET indicates that we are using IPv4
-        - type: SOCK_STREAM indicates that we are using a
-       connection-oriented stream protocol (TCP)
-        - protocol: indicates the protocol to be used with the
-       socket. A value of 0 indicates that the default protocol
-       (TCP) will be used. */
-    int server_socket_descriptor =
-        socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket_descriptor < 0) {
-        perror("Error while creating socket");
-        exit(1);
-    }
+typedef struct {
+    int socket;
+    char id[MAX_IDENTIFIER_LEN];
+    int id_len;
+} client_t;
 
-    /* struct sockaddr_in
-        short            sin_family;   - this is always set to
-       AF_INET because sockaddr_in is always used with IPv4
-        unsigned short   sin_port;     - port number
-        struct in_addr   sin_addr;     - IP address on which the
-       server will listen. INADDR_ANY means that the server will
-       listen on all available interfaces of the running machine
-        char             sin_zero[8];  - this is padding to make
-       struct the same size as sockaddr */
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-    /* htons() converts the unsigned short integer hostshort
-     * from host byte order to big-endian network byte order
-     * (commonly used in networking protocols) */
-    server_addr.sin_port = htons(PORT);
+client_t clients[MAX_CLIENTS];
 
-    /* bind(socket_descriptor, address structure,
-     * address_length) - assigns the address to the socket */
-    if (bind(
-            server_socket_descriptor,
-            (struct sockaddr *)&server_addr,
-            sizeof(server_addr)
-        ) < 0) {
-        perror("Error while binding socket");
-        close(server_socket_descriptor);
-        exit(1);
-    }
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-    /* listen(socket_descriptor, backlog) - marks the socket as
-       a passive socket, that is, as a socket that will be used
-       to accept incoming connection requests using accept
-        - backlog: the maximum length to which the queue of
-       pending connections for socket_descriptor may grow */
-    if (listen(server_socket_descriptor, 5) < 0) {
-        perror("Error while listening");
-        close(server_socket_descriptor);
-        exit(1);
-    }
+void *handler(int client_socket) {
+    char id[MAX_IDENTIFIER_LEN];
+    char buff[BUFFER_SIZE];
 
-    printf("Server is listening on port %d\n", PORT);
+    int id_len = recv(
+        client_socket, id, MAX_IDENTIFIER_LEN, 0
+    ); // CHECK IF NOT &id
 
-    struct user {
-        int fd;
-        char *client_id;
-    };
+    pthread_mutex_lock(&clients_mutex);
 
-    struct user users[MAX_CLIENTS];
-    struct pollfd poll_fds[MAX_CLIENTS + 1];
-
-    char buffer[MESSAGE_SIZE];
-    int client_socket;
-
-    for (int i = 0; i < MAX_CLIENTS + 1; i++) {
-        poll_fds[i].fd = -1;
-    }
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        users[i].fd = -1;
-        users[i].client_id = (char *)malloc(256);
-    }
-
-    poll_fds[0].fd = server_socket_descriptor;
-    poll_fds[0].events = POLLIN;
-
-    while (1) {
-        // poll(struct pollfd fds[], nfds_t nfds, int timeout) -
-        // waits for one of a set of file descriptors to become
-        // ready to perform i/o
-        if (poll(poll_fds, MAX_CLIENTS + 1, -1) < 0) {
-            perror("error while polling");
-            close(server_socket_descriptor);
-            exit(1);
+    for (int i = 0; i < MAX_IDENTIFIER_LEN; i++) {
+        if (clients[i].socket == 0) {
+            clients[i].socket = client_socket;
+            strcpy(clients[i].id, id);
+            clients[i].id_len = id_len;
+            break;
         }
-        // checks if there is a new connection
-        if (poll_fds[0].revents & POLLIN) {
-            struct sockaddr_in client_addr;
-            socklen_t addr_len;
+    }
 
-            client_socket = accept(
-                server_socket_descriptor,
-                (struct sockaddr *)&client_addr,
-                &addr_len
-            );
-            if (client_socket < 0) {
-                perror("error while accepting connection");
-                continue;
-            }
-            printf(
-                "new connection, socket fd: %d, ip: %s, port: "
-                "%d\n",
-                client_socket,
-                inet_ntoa(client_addr.sin_addr),
-                ntohs(client_addr.sin_port)
-            );
+    pthread_mutex_unlock(&clients_mutex);
 
-            for (int i = 1; i <= MAX_CLIENTS; i++) {
-                if (i == MAX_CLIENTS) {
-                    printf("max clients reached. rejecting "
-                           "connection...\n");
-                    close(client_socket);
+    while (g_keep_alive) {
+        int received =
+            recv(client_socket, buff, BUFFER_SIZE, 0); // &buff?
+        if (received == -1)
+            perror("recv");
+
+        if (strncmp(buff, "LIST", 4) == 0) {
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i].socket != 0) {
+                    send(
+                        client_socket,
+                        clients[i].id,
+                        clients[i].id_len,
+                        0
+                    );
+                    send(client_socket, "\n", 1, 0);
                 }
-                if (poll_fds[i].fd == -1) {
-                    poll_fds[i].fd = client_socket;
-                    poll_fds[i].events = POLLIN;
+            }
+        } else if (strncmp(buff, "2ALL", 4) == 0) {
+            char message[BUFFER_SIZE + 50];
+            time_t now = time(0);
+            strftime(
+                message, 50, "[%Y-%m-%d %H:%M:%S] ", localtime(&now)
+            );
+            strcat(message, id);
+            strcat(message, ": ");
+            strcat(message, buff + 5);
+
+            pthread_mutex_lock(&clients_mutex);
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i].socket != 0 &&
+                    clients[i].socket != client_socket) {
+                    send(
+                        clients[i].socket,
+                        &message,
+                        strlen(message),
+                        0
+                    );
+                }
+            }
+            pthread_mutex_unlock(&clients_mutex);
+        } else if (strncmp(buff, "2ONE", 4) == 0) {
+            char *recipient_id = strtok(buff + 5, " ");
+            char *message = strtok(NULL, "");
+            char full_message[BUFFER_SIZE + 50];
+            time_t now = time(0);
+            strftime(
+                full_message,
+                50,
+                "[%Y-%m-%d %H:%M:%S] ",
+                localtime(&now)
+            );
+            strcat(full_message, id);
+            strcat(full_message, ": ");
+            strcat(full_message, message);
+            pthread_mutex_lock(&clients_mutex);
+            for (int i = 0; i < MAX_CLIENTS; ++i) {
+                if (clients[i].socket != 0 &&
+                    strcmp(clients[i].id, recipient_id) == 0) {
+                    send(
+                        clients[i].socket,
+                        full_message,
+                        strlen(full_message),
+                        0
+                    );
                     break;
                 }
             }
+            pthread_mutex_unlock(&clients_mutex);
+        } else if (strncmp(buff, "STOP", 4) == 0) {
+            break;
         }
+    }
 
-        for (int i = 1; i < MAX_CLIENTS; i++) {
-            if (poll_fds[i].fd != -1 &&
-                (poll_fds[i].revents & POLLIN)) {
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i].socket == client_socket) {
+            clients[i].socket = 0;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
 
-                memset(buffer, 0, sizeof(buffer));
-                int bytes_read = read(
-                    poll_fds[i].fd, buffer, sizeof(buffer) - 1
-                );
-                if (bytes_read <= 0) {
-                    if (bytes_read == 0) {
-                        printf(
-                            "Client disconnected, socket fd: "
-                            "%d\n",
-                            poll_fds[i].fd
-                        );
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd == poll_fds[i].fd) {
-                                users[j].fd = -1;
-                                break;
-                            }
-                        }
-                    } else {
-                        perror("Error while reading data from "
-                               "client");
-                    }
-                    close(poll_fds[i].fd);
-                    poll_fds[i].fd = -1;
-                } else {
-                    buffer[bytes_read] = '\0';
-                    char *command = get_command(buffer);
-                    if (strcmp(command, SETID_COMMAND) == 0) {
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd == -1) {
-                                users[j].fd = poll_fds[i].fd;
-                                strcpy(
-                                    users[j].client_id,
-                                    get_string_after_command(
-                                        buffer
-                                    )
-                                );
-                                break;
-                            }
-                        }
-                    } else if (strcmp(command, TO_ALL_COMMAND) == 0) {
-                        char sender[CLIENT_ID_SIZE];
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd != -1 &&
-                                poll_fds[i].fd != -1 &&
-                                users[j].fd == poll_fds[i].fd) {
-                                strcpy(
-                                    sender, users[j].client_id
-                                );
-                                break;
-                            }
-                        }
-                        char message[TOTAL_MESSAGE_SIZE];
-                        sprintf(
-                            message,
-                            "%s> %s",
-                            sender,
-                            get_string_after_command(buffer)
-                        );
-                        for (int j = 1; j < MAX_CLIENTS; j++) {
-                            if (poll_fds[j].fd != -1 &&
-                                poll_fds[j].fd !=
-                                    poll_fds[i].fd) {
-                                write(
-                                    poll_fds[j].fd,
-                                    message,
-                                    strlen(message) + 1
-                                );
-                            }
-                        }
-                    } else if (strcmp(command, LIST_COMMAND) == 0) {
-                        char message[TOTAL_MESSAGE_SIZE];
-                        strcpy(message, "Users: ");
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd != -1) {
-                                strcat(
-                                    message, users[j].client_id
-                                );
-                                strcat(message, " ");
-                            }
-                        }
-                        write(
-                            poll_fds[i].fd,
-                            message,
-                            strlen(message) + 1
-                        );
-                    } else if (strcmp(command, TO_ONE_COMMAND) == 0) {
-                        char *message =
-                            get_string_after_param(buffer);
-                        char *recipient =
-                            get_param_after_command(buffer);
-                        char sender[CLIENT_ID_SIZE];
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd != -1 &&
-                                poll_fds[i].fd != -1 &&
-                                users[j].fd == poll_fds[i].fd) {
-                                strcpy(
-                                    sender, users[j].client_id
-                                );
-                                break;
-                            }
-                        }
-                        char total_message[TOTAL_MESSAGE_SIZE];
-                        time_t t;
-                        time(&t);
-                        char time_str[32];
-                        strcpy(time_str, ctime(&t));
-                        char *end = strchr(time_str, '\n');
-                        if (end != NULL) {
-                            *end = '\0';
-                        }
-                        sprintf(
-                            total_message,
-                            "%s: %s> %s",
-                            time_str,
-                            sender,
-                            message
-                        );
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd != -1 &&
-                                strcmp(
-                                    users[j].client_id,
-                                    recipient
-                                ) == 0) {
-                                write(
-                                    users[j].fd,
-                                    total_message,
-                                    strlen(total_message) + 1
-                                );
-                                break;
-                            }
-                        }
-                    } else if (strcmp(command, STOP_COMMAND) == 0) {
-                        for (int j = 0; j < MAX_CLIENTS; j++) {
-                            if (users[j].fd == poll_fds[i].fd) {
-                                users[j].fd = -1;
-                                break;
-                            }
-                        }
-                        close(poll_fds[i].fd);
-                        poll_fds[i].fd = -1;
-                    }
-                    memset(buffer, 0, sizeof(buffer));
+    close(client_socket);
+    pthread_exit(0);
+}
+
+void *check_alive_clients(void *a) {
+    UNUSED(a);
+    while (1) {
+        sleep(3);
+        pthread_mutex_lock(&clients_mutex);
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].socket != 0) {
+                if (send(clients[i].socket, "ALIVE", 5, 0) <= 0) {
+                    close(clients[i].socket);
+                    clients[i].socket = 0;
                 }
             }
         }
+        pthread_mutex_unlock(&clients_mutex);
     }
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        free(users[i].client_id);
-    }
-    close(server_socket_descriptor);
+    return NULL;
+}
 
-    return 0;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Please provide ip and port\n");
+        return EXIT_FAILURE;
+    }
+
+    // socket
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
+        perror("socket");
+
+    // bind
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port =
+        htons(atoi(argv[2])); // potentially its not htons,
+                              // since atoi could return 32-bit
+    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        return 1;
+    }
+
+    if (bind(
+            server_socket,
+            (struct sockaddr *)&server_addr,
+            sizeof(server_addr)
+        ) == -1)
+        perror("bind");
+
+    // listen
+    if (listen(server_socket, MAX_CLIENTS) == -1)
+        perror("liten");
+
+    pthread_t thread_id;
+    int client_socket;
+
+    pthread_create(&thread_id, NULL, check_alive_clients, NULL);
+
+    while (g_keep_alive) {
+        // accept
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        client_socket = accept(
+            server_socket,
+            (struct sockaddr *)&client_addr,
+            &client_addr_len
+        );
+        printf("Accepted someone\n");
+        if (client_socket == -1)
+            perror("accept");
+
+        pthread_create(
+            &thread_id,
+            NULL,
+            (void *)handler,
+            (void *)(intptr_t)client_socket
+        );
+    }
+
+    if (shutdown(server_socket, SHUT_RDWR) == -1)
+        perror("shutdown");
+    if (close(server_socket) == -1)
+        perror("close");
+
+    printf("Hello\n");
+
+    return EXIT_SUCCESS;
 }
