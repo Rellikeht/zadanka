509bc34aabb25c6cbaa8dd7ad32562040dc5ccc1
Rellikeht <michaelhemperek@gmail.com>
2025-01-10 23:25:11 +0100

    make single threaded crawler work

--- tw/lab10/app/src/main/scala/webcrawler/WebCrawler.scala
+++ tw/lab10/app/src/main/scala/webcrawler/WebCrawler.scala
@@ -1,46 +1,71 @@
 package webcrawler
 
+import java.nio.file.FileAlreadyExistsException
+
 object WebCrawler {
   import scala.io.Source
   import org.htmlcleaner.HtmlCleaner
   import java.net.URL
-  val defaultLevel = 5
+  import java.nio.file.Paths
+  import java.nio.file.Files
+  import scala.collection.mutable
 
-  def crawl(level: Int = defaultLevel) = {
-    // println(sys.props.get("user.dir"))
-    val path = Paths.get(".", "html_files")
-    println(path)
-    // if(!(Files.exists(path) && Files.isDirectory(path)))
-    Files.createDirectory(path)
-
-    println()
-    return
-
-    val url = "http://google.com"
-    val html = Source.fromURL(url)
-
-    val cleaner = new HtmlCleaner
-    val props = cleaner.getProperties
-
-    val rootNode = cleaner.clean(html.mkString)
-    // val rootNode = cleaner.clean(new URL(url))
-
-    val elements = rootNode.getElementsByName("a", true)
-    elements map { elem =>
-      val url = elem.getAttributeByName("href")
-      println(url.toString)
+  private val defaultLevel = 2
+  private val visited = mutable.Set[String]()
+  private val path = Paths.get(".", "html_files")
+  if (Files.exists(path)) {
+    if (!Files.isDirectory(path)) {
+      throw new FileAlreadyExistsException(
+        "Cannot create directory for html files"
+      )
     }
+  } else Files.createDirectory(path)
+
+  private def download(url: String, level: Int): Unit = {
+    // println(url)
+    if (level <= 0 || visited(url)) return
+    visited += url
+    println("visiting " + url)
+
+    try {
+      val cleanUrl = url
+        .replace("https://", "")
+        .replace("http://", "")
+        .replace("/", "_")
+      // println(cleanUrl)
+      val fpath = Paths.get(path.normalize().toString(), cleanUrl)
+      val html = Source.fromURL(url)
+      val htmlString = html.mkString
+      Files.write(fpath, htmlString.getBytes())
+
+      val cleaner = new HtmlCleaner
+      val props = cleaner.getProperties()
+      val rootNode = cleaner.clean(htmlString)
+
+      val elements = rootNode.getElementsByName("a", true)
+      elements map { elem =>
+        val url = elem.getAttributeByName("href")
+        download(url, level - 1)
+      }
+    } catch {
+      case e: Exception => return
+    }
+  }
+
+  def crawl(url: String, level: Int = defaultLevel) = {
+    download(url, level)
   }
 }
 
 object App {
   def main(args: Array[String]): Unit = {
-    // val level = if (args.length > 0) args(0).toInt else WebCrawler.defaultLevel
-    // WebCrawler.crawl(level)
+    val url = "http://google.com"
+    // val url = "https://suckless.org"
+
     if (args.length > 0) {
-      WebCrawler.crawl(args(0).toInt)
+      WebCrawler.crawl(url, args(0).toInt)
     } else {
-      WebCrawler.crawl()
+      WebCrawler.crawl(url)
     }
   }
 }
