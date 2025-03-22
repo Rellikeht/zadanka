6f3cbeb6026194b5c29e9fc8d3ac1477b3a8aaf1
Rellikeht <michaelhemperek@gmail.com>
2025-03-22 19:16:45 +0100

    add almost working xkcd

--- rozprochy/rest/main.go
+++ rozprochy/rest/main.go
@@ -2,25 +2,77 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-// fetches random joke
-func randomJoke() {
-	resp, err := http.Get("https://official-joke-api.appspot.com/jokes/random")
-	if err != nil {
-		log.Fatal(err)
-	}
-	reader := bufio.NewReader(resp.Body)
-	fmt.Println(reader.ReadString('\n'))
+var (
+  comic_img string
+  resources = make(map[string]string)
+)
+
+func handleResource(name string) func(writer http.ResponseWriter, request *http.Request) {
+  return func(writer http.ResponseWriter, request *http.Request) {
+    writer.Write([]byte(resources[name]))
+  }
+}
+
+func xkcd(number int) error {
+	address := fmt.Sprintf("https://xkcd.com/%d/info.0.json", number)
+	response, err := http.Get(address)
+  if err != nil {
+    return err
+  }
+
+  var (
+    content any
+    // bytes []byte
+  )
+	reader := bufio.NewReader(response.Body)
+  bytes, _ := reader.ReadString('\n')
+  // if err != nil {
+  //   log.Println(err)
+  //   return err
+  // }
+  err = json.Unmarshal([]byte(bytes), &content)
+  if err != nil {
+    return err
+  }
+
+  data := content.(map[string]any)
+  comic_img = data["img"].(string)
+  resources["comic-alt"] = data["alt"].(string)
+
+  return nil
+}
+
+func handleComic(writer http.ResponseWriter, request *http.Request) {
+  id, err := strconv.Atoi(request.URL.Query().Get("xkcd-id"))
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  err = xkcd(id)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  http.Redirect(writer, request, "/comic.html", http.StatusFound)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.FileServer(http.Dir("./static")))
+	mux.HandleFunc("/get-comic", handleComic)
+
+	mux.Handle("/comic-img", http.RedirectHandler(comic_img, http.StatusFound))
+  for _, name := range []string{"comic-alt", "joke"} {
+    mux.HandleFunc("/"+name, handleResource(name))
+  }
 
 	err := http.ListenAndServe(":9009", mux)
 	if err != nil {
