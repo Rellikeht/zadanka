a85a9e62ef1d696bc4f20f29dd0dc29eea381e5b
Rellikeht <michaelhemperek@gmail.com>
2025-03-22 23:12:26 +0100

    make xkcd work

--- rozprochy/rest/main.go
+++ rozprochy/rest/main.go
@@ -10,57 +10,68 @@ import (
 )
 
 var (
-  comic_img string
-  resources = make(map[string]string)
+	comic_img string
+	resources = make(map[string]string)
 )
 
 func handleResource(name string) func(writer http.ResponseWriter, request *http.Request) {
-  return func(writer http.ResponseWriter, request *http.Request) {
-    writer.Write([]byte(resources[name]))
-  }
+	return func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(resources[name]))
+	}
 }
 
 func xkcd(number int) error {
 	address := fmt.Sprintf("https://xkcd.com/%d/info.0.json", number)
 	response, err := http.Get(address)
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  var (
-    content any
-    // bytes []byte
-  )
+	var (
+		content any
+		// bytes []byte
+	)
 	reader := bufio.NewReader(response.Body)
-  bytes, _ := reader.ReadString('\n')
-  // if err != nil {
-  //   log.Println(err)
-  //   return err
-  // }
-  err = json.Unmarshal([]byte(bytes), &content)
-  if err != nil {
-    return err
-  }
+	bytes, _ := reader.ReadString('\n')
+	// if err != nil {
+	//   log.Println(err)
+	//   return err
+	// }
+	err = json.Unmarshal([]byte(bytes), &content)
+	if err != nil {
+		return err
+	}
 
-  data := content.(map[string]any)
-  comic_img = data["img"].(string)
-  resources["comic-alt"] = data["alt"].(string)
+	data := content.(map[string]any)
+	comic_img = data["img"].(string)
+	resources["comic-alt"] = data["alt"].(string)
 
-  return nil
+	return nil
+}
+
+func joke(contains string) error {
+	// https://v2.jokeapi.dev/joke/Any?
+	// blacklistFlags=nsfw,religious,political,racist,sexist&contains=%s
+	return nil
 }
 
 func handleComic(writer http.ResponseWriter, request *http.Request) {
-  id, err := strconv.Atoi(request.URL.Query().Get("xkcd-id"))
-  if err != nil {
-    log.Println(err)
-    return
-  }
-  err = xkcd(id)
-  if err != nil {
-    log.Println(err)
-    return
-  }
-  http.Redirect(writer, request, "/comic.html", http.StatusFound)
+	id, err := strconv.Atoi(request.URL.Query().Get("xkcd-id"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = xkcd(id)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = joke(resources["joke"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	http.Redirect(writer, request, "/comic.html", http.StatusSeeOther)
 }
 
 func main() {
@@ -68,11 +79,13 @@ func main() {
 
 	mux.Handle("/", http.FileServer(http.Dir("./static")))
 	mux.HandleFunc("/get-comic", handleComic)
-
-	mux.Handle("/comic-img", http.RedirectHandler(comic_img, http.StatusFound))
-  for _, name := range []string{"comic-alt", "joke"} {
-    mux.HandleFunc("/"+name, handleResource(name))
-  }
+	mux.HandleFunc("/comic-img",
+		func(writer http.ResponseWriter, request *http.Request) {
+			http.Redirect(writer, request, comic_img, http.StatusSeeOther)
+		})
+	for _, name := range []string{"comic-alt", "joke"} {
+		mux.HandleFunc("/"+name, handleResource(name))
+	}
 
 	err := http.ListenAndServe(":9009", mux)
 	if err != nil {
