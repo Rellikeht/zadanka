7d80fbe8d2d54d50123367478b4dbb092ffa557c
Rellikeht <michaelhemperek@gmail.com>
2025-04-13 20:45:16 +0200

    add code that can be optimized more

--- optymalizacja/zadanie1/wersja1.cpp
+++ optymalizacja/zadanie1/wersja1.cpp
@@ -5,9 +5,9 @@ using namespace chrono;
 
 const char LOWERCASE = 32;
 
+// TODO more bulk comparisons?
 static inline void remove_duplicates(string &input) {
-
-  size_t input_pos = 0, output_pos = 0;
+  long long input_pos = 0, output_pos = 0;
   bool space = false, capital = false;
 
   while (input_pos < input.size()) {
@@ -22,21 +22,27 @@ static inline void remove_duplicates(string &input) {
         output_pos++;
       }
       break;
+
     case '.':
     case ',':
     case ':':
     case ';':
+    case '!':
+    case '?':
       input[output_pos] = ',';
       output_pos++;
       space = false;
       break;
+
     case 'A' ... 'Z':
       capital = true;
-    case '!' ... '+':
+      // fallthrough
+    case '"' ... '+':
     case '-':
     case '/':
+    case '@':
     case '0' ... '9':
-    case '<' ... '@':
+    case '<' ... '>':
     case 'a' ... 'z':
     case '[' ... '`':
     case '{' ... '~':
@@ -49,12 +55,74 @@ static inline void remove_duplicates(string &input) {
       output_pos++;
       break;
     default:
-      space = false;
+      /* space = false; */
+      break;
     }
     input_pos++;
   }
 
-  // TODO remove duplicates
+  input.resize(output_pos);
+  if (input.size() < 2) {
+    return;
+  }
+
+  input_pos = 0;
+  output_pos = 0;
+  long long word_length = 0, prev_word = 0, cur_word = 0;
+  bool duplicate = false;
+
+  while (input_pos < input.size()) {
+    if (input[input_pos] == ' ') {
+      input_pos++;
+      break;
+    }
+    input_pos++;
+    output_pos++;
+  }
+
+  while (input_pos < input.size()) {
+    if (input[input_pos] == ' ') {
+      prev_word = output_pos - word_length - 1;
+      cur_word = input_pos - word_length - 1;
+      /* cerr << cur_word << ' ' << word_length << ' '; */
+      /* cerr << output_pos << ' ' << input_pos << '\n'; */
+
+      if (prev_word < 0) {
+        cur_word++;
+        // TODO better copying
+        while (cur_word <= input_pos) {
+          output_pos++;
+          input[output_pos] = input[cur_word];
+          cur_word++;
+        }
+      } else {
+
+        duplicate = true;
+        while (cur_word < input_pos - 1) {
+          if (input[prev_word] != input[cur_word]) {
+            duplicate = false;
+            break;
+          }
+          prev_word++;
+          cur_word++;
+        }
+        if (!duplicate) {
+          // TODO better copying
+          for (cur_word = input_pos - word_length;
+               cur_word <= input_pos;
+               cur_word++) {
+            output_pos++;
+            input[output_pos] = input[cur_word];
+          }
+        }
+      }
+
+      word_length = 0;
+    } else {
+      word_length++;
+    }
+    input_pos++;
+  }
 
   input.resize(output_pos);
 }
@@ -65,22 +133,26 @@ int main() {
   cout.tie(nullptr);
 
   string input, tmp;
-  while (cin >> tmp) {
+  while (getline(cin, tmp)) {
     input += tmp + "\n";
   }
 
   const int RUNS = 10;
   long long elapsed_time = 0;
+  string work_input = input;
+  remove_duplicates(work_input);
 
-  for (int i = 1; i < RUNS; i++) {
+  for (int i = 0; i < RUNS; i++) {
+    work_input = input;
     auto start_time = high_resolution_clock::now();
-    remove_duplicates(input);
+    remove_duplicates(work_input);
     auto end_time = high_resolution_clock::now();
     elapsed_time +=
         duration_cast<microseconds>(end_time - start_time).count();
   }
+  elapsed_time /= RUNS;
 
   cout << "Time elapsed: " << elapsed_time << "μs\n";
-  // cout << input << "\n";
+  cout << work_input << "\n";
   return 0;
 }
