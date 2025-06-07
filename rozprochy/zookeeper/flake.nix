78a588a2a4a89dea46ab5cd4d1ec55e11d9763db
Rellikeht <michaelhemperek@gmail.com>
2025-06-07 19:45:12 +0200

    change language for zookeeper

--- rozprochy/zookeeper/flake.nix
+++ rozprochy/zookeeper/flake.nix
@@ -1,45 +1,41 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-24.05";
     flakeUtils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    # nixpkgs-old,
     flakeUtils,
   }:
     flakeUtils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        # pkgs-old = nixpkgs-old.legacyPackages.${system};
 
         buildInputs = with pkgs; [
-          gradle
-          jdk17
-
+          python313Full
+          uv
           zookeeper
         ];
-        # ++ (with pkgs-old; [
-        #   zookeeper
-        # ]);
-        # ++ (with self.packages.${system}; [default]);
+
+        libs = with pkgs; [
+          #  {{{
+          zlib
+          stdenv.cc.cc.lib
+          glib
+        ]; #  }}}
+        ld_path = "${pkgs.lib.makeLibraryPath libs}";
       in {
         devShells = {
           default = pkgs.mkShell {
             inherit buildInputs;
             phases = [];
-            shellHook = '''';
+            shellHook = ''
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ld_path}"
+            '';
           };
         };
-
-        # packages = rec {
-        #   default = pkgs.writeScriptBin "run" ''
-        #     echo "Do the thing"
-        #   '';
-        # };
       }
     );
 }
